0668b1eb3b0cf534e486aff4c41b4828f45c92e8
guillaume <guillaume.lorin@insa-cvl.fr>
2023-06-13 18:13:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//On import le module express
+const express = require("express");
+
+//on instancie express dans une constante app
+const app = express();
+
+//on importe le module path pour définir le chemin des fichiers statiques
+const path = require("path");
+
+//on dit à app d'utiliser le module express.urlencoded pour pouvoir récupérer les données du formulaire
+app.use(express.urlencoded({ extended: false }));
+
+//On permet à express d'utiliser le dossier public pour les fichiers statiques
+app.use(express.static(path.join(__dirname, "public")));
+
+//on défini le port sur lequel le serveur va écouter
+const port = 4000;
+
+//on importe le module http pour pouvoir créer un serveur qui va utiliser express
+const http = require("http").createServer(app);
+
+//on importe le module socket.io pour pouvoir utiliser les websockets
+const io = require("socket.io")(http);
+
+//renvoie à la page connection.html lorsque l'on accède à la racine du serveur
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/pages/connexion.html"));
+});
+
+//On demande au serveur d'écouter sur le port défini plus haut
+http.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
