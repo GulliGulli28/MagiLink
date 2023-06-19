127b7aef2be4120565be85697fcdfe345d605423
guillaume <guillaume.lorin@insa-cvl.fr>
2023-06-19 11:30:52 +0200

    register client ok

--- public/js/inscription.js
+++ public/js/inscription.js
@@ -15,6 +15,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
   } else {
     const password = document.getElementById("password").value;
     const confirm_password = document.getElementById("confirm-password").value;
+    console.log(password.length);
     if (password == "") {
       console.log("password must be set");
       event.preventDefault();
@@ -26,6 +27,11 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
         if (password != confirm_password) {
           console.log("Pas le même password");
           event.preventDefault();
+        } else {
+          if (password.length < 12) {
+            console.log("Password must be at least 12 caracteres long");
+            event.preventDefault();
+          }
         }
       }
     }
