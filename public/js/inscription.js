1114316f0c99dcb58dfd9137e8ad9e81d41a0107
guillaume <guillaume.lorin@insa-cvl.fr>
2023-06-19 11:16:45 +0200

    vérification client du register form ok

--- public/js/inscription.js
+++ public/js/inscription.js
@@ -4,24 +4,28 @@ function verifierEmail(email) {
 }
 
 document.getElementById("myForm").addEventListener("submit", function (event) {
-  event.preventDefault(); // Empêche le comportement par défaut de l'envoi du formulaire
+  // Empêche le comportement par défaut de l'envoi du formulaire
   const mail = document.getElementById("mail").value;
   const checkmail = verifierEmail(mail);
   console.log(checkmail);
 
   if (!checkmail) {
     console.log("invalide");
+    event.preventDefault();
   } else {
     const password = document.getElementById("password").value;
     const confirm_password = document.getElementById("confirm-password").value;
     if (password == "") {
       console.log("password must be set");
+      event.preventDefault();
     } else {
       if (confirm_password == "") {
         console.log("password must be confirmed");
+        event.preventDefault();
       } else {
         if (password != confirm_password) {
           console.log("Pas le même password");
+          event.preventDefault();
         }
       }
     }
